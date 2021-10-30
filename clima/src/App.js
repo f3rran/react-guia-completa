99dea7c277d9fba2fca2fbb1773132910cb66bb4
f3rran <brechyt@gmail.com>
2021-10-30 19:40:04 +0200

    Avanzado proyecto del clima

---
+++ clima/src/App.js
@@ -0,0 +1,58 @@
+import React, {Fragment, useState, useEffect} from 'react';
+import Header from './components/Header';
+import Formulario from './components/Formulario';
+
+function App() {
+
+  //State del formulario
+  const [busqueda, setbusqueda] = useState({
+      ciudad: '',
+      pais: ''
+  });
+  const [consultar, guardarConsultar] = useState(false);
+
+  const {ciudad, pais} = busqueda;
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      if (consultar) {
+        const appId = '5361a2d736df51c036427bdb14ba604a';
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+
+        console.log(resultado);
+      }
+    };
+    consultarAPI();
+  }, [consultar])
+
+  return (
+    <Fragment>
+      <Header 
+        titulo='Clima React App'
+      />
+
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col m6 s12">
+              <Formulario 
+                busqueda={busqueda}
+                setbusqueda={setbusqueda}
+                guardarConsultar={guardarConsultar}
+              />
+
+            </div>
+            <div className="col m6 s12">
+2
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
