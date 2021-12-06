3efaa9ebf1737b9d775f3e73650ca8d0ee7211b9
f3rran <brechyt@gmail.com>
2021-12-06 13:32:20 +0100

    Inicializado proyecto Redux

---
+++ crudredux/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Header from './components/Header';
+import Products from './components/Products';
+import NewProduct from './components/NewProduct';
+import EditProduct from './components/EditProduct';
+
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <Header/>
+
+      <div className="container mt-5">
+        <Routes>
+          <Route exact path="/" element={<Products/>} />
+          <Route exact path="/products/new" element={<NewProduct/>} />
+          <Route exact path="/products/edit/:id" element={<EditProduct/>} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
